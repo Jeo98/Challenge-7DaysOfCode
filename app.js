4dc1ad3660674bf5dccd0b7ccec0715dd3b28242
unknown <jeo98dev@gmail.com>
2024-08-18 12:39:19 -0300

    dia 2 desafio

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let numeroDiez = 10
 
 let stringDiez = '10'
 
-
+/*
 if ( numeroUn == stringUn) {
 
     console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes')
@@ -43,6 +43,20 @@ if (numeroTreinta === stringTreinta) {
   
     console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor')
   
-  }
+  }*/
 
 
+//segundo dia, segundo desafio
+
+
+const nombre=prompt("¿Cuál es tu nombre?");
+const edad=prompt("¿Cuántos años tienes?");
+const lenguaje=prompt("¿Qué lenguaje de programación estás estudiando?");
+
+alert(`Hola ${nombre}!, tienes ${edad} y estas estudiando el lenguaje - ${lenguaje}`);
+
+const opcional=prompt("¿Te gusta estudiar [lenguaje]? Responde con el número 1 para SÍ o 2 para NO");
+if(opcional==1)
+    alert("¡Muy bien! Sigue estudiando y tendrás mucho éxito.");
+else
+    alert(prompt("Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?"))
